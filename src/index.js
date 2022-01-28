0ce4718f1edf9d434da62655126b8b3ab67f03c9
Jaqueline de Oliveira <jaquelinedeoliveiraa@gmail.com>
2022-01-28 11:10:26 -0300

    tentativa de aplicar o algoritmo de Luhn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import validator from './validator.js';
 
+let campoCartao = document.getElementById("cartao");
+let numeroCartao = campoCartao.value;
+
+const finalizar = document.getElementById("botao");
+finalizar.addEventListener("click", validator);
+
+let soma = 0;
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,7 @@
 const validator = {
-  // ...
-};
-
+    for (let i = 0, i < numeroCartao.length, i++){
+        let digitosCartao = numeroCartao[i],
+        console.log(numeroCartao);
+    }
+}
 export default validator;
