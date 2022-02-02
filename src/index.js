52be969f05ae30768593cf95e301fd6a7f264014
Jaqueline de Oliveira <jaquelinedeoliveiraa@gmail.com>
2022-02-02 11:02:50 -0300

    segunda tentativa de fazer o algoritmo de luhn

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-//import validator from './validator.js';//
+/*import validator from './validator.js';*/
 
 const botaoFinalizar = document.getElementById("botao");
-botaoFinalizar.addEventListener("click", construindoArray);
+botaoFinalizar.addEventListener("click", validator);
 
-function construindoArray(){
-    let numeroCartao = document.getElementById("cartao").value;
+function validator(){  
+let numeroCartao = document.getElementById("cartao").value;
+const digitos = Array.from(numeroCartao);
+let digitosInvertidos = digitos.reverse();
+const somaTotal = 0;
 
-    for (let index = 0; index < numeroCartao.length; index++) {
-        let conjuntoDigitos = numeroCartao[index];
-        let digitosConvertidos = conjuntoDigitos.toString().splint("").reverse();
-        console.log(digitosConvertidos);
-    }
+for (let index = 1; index < digitosInvertidos.length; index++){
+  if (index % 2 === 0)
+    if ((digitosInvertidos *= 2) > 9)
+          digitosInvertidos -= 9;
 
+  somaTotal += digitosInvertidos;
+  return (somaTotal % 10 === 0)
+
+if (somaTotal === 0){
+    alert("CC valid");
+  }else{
+    aler("CC invalid");
+  }
+}
 }
 
-//console.log(validator());//
+/*console.log(validator());*/
--- src/validator.js
+++ src/validator.js
@@ -1,31 +1,6 @@
+/*const validator = {
 
-/**
- function validator(numeroCartao) {
-    for (let i = 0; i < numeroCartao.length; i++){
-        let Digitos = numeroCartao[i];
-        let converteDigitos = Digitos.toString();
-        Digitos.revarse();
-
-        
-          if (o numero for impar não faça nada)
-          if ((digitosDoCartao *= 2) > 9)
-          digitosDoCartao -= 9
-         
-        soma += digitosDoCartao,
-
-        return (soma % 10) === 0;
-        
-
-         
-};
+}*/
 
 
-    if (luhn (numeroCartao)){
-        alert("Cartão de crédito Valido");
-    }else{
-        alert("Cartão de crédito Invalido");
-    } 
-}
-
-export default validator;
-*/
+/*export default validator;*/
