3d85d62273eb501500c53046544e664771038026
Jaqueline de Oliveira <jaquelinedeoliveiraa@gmail.com>
2022-02-06 11:58:56 -0300

    alterações na lógica do algoritmo de Luhn

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import validator from './validator.js';
 
 const botaoFinalizar = document.getElementById("botao");
-botaoFinalizar.addEventListener("click", finalizar);
+botaoFinalizar.addEventListener("click", verificarCartao);
 
-
-
-function finalizar(){
-let numeroCartao = document.getElementById("cartao").value;
-let resultado = validator.isValid(numeroCartao);
-if (resultado === true){
-  alert("cc is valid");
-}else{
-  alert("cc invalido")
-}
+function verificarCartao(event) {
+  event.preventDefault()
+  let numeroCartao = document.getElementById("cartao").value;
+  if (numeroCartao.length >= 12 && numeroCartao.length <= 16){
+    let resultado = validator.isValid(numeroCartao);
+    if (resultado === true) {
+      alert("Cartao de credito é valido");
+    } else {
+      alert("Cartao de credito é invalido")
+    }
+  } else {
+    alert("Quantidades de digitos insuficiente")
+  }
 }
--- src/validator.js
+++ src/validator.js
@@ -1,23 +1,33 @@
 const validator = {
-    isValid (numeroCartao){  
-        let digitos = Array.from(numeroCartao);
-        let digitosInvertidos = digitos.reverse();
-        let somaTotal = 0;
-        let dobroDigitos = 0;
 
-        for (let index = 1; index < digitosInvertidos.length; index++){
-          if (index % 2 === 1){
-            digitosInvertidos[index] = digitosInvertidos[index] *= 2;
-          }if (digitosInvertidos[index] > 9){
-            digitosInvertidos[index] -= 9
-            somaTotal += digitosInvertidos[index]
-          }if (somaTotal % 10 === 0){
-              return true
-          }else{
-              return false
-          }
+  isValid(numeroCartao) {
+
+    let digitos = Array.from(numeroCartao)
+    let digitosInvertidos = digitos.reverse()
+    let soma = 0
+
+    for (let index = 0; index < digitosInvertidos.length; index++) {
+      if (index % 2 != 0) {
+        digitosInvertidos[index] = (Number(digitosInvertidos[index])) * 2
+        if (digitosInvertidos[index] > 9) {
+          digitosInvertidos[index] = digitosInvertidos[index] - 9
+          soma = soma + Number(digitosInvertidos[index])
         }
+        else {
+          soma = soma + (digitosInvertidos[index])
         }
+      }
+      else {
+        soma = soma + Number(digitosInvertidos[index])
+      }
     }
+    if (soma % 10 === 0) {
+      return true
 
-export default validator;
+    } else {
+      return false
+    }
+  }
+}
+  
+  export default validator;
